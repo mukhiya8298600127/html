bdd22aaad3e36d6f04bedf146aa35efbbf41a1c1
mukhiya8298600127 <157529067+mukhiya8298600127@users.noreply.github.com>
2025-01-10 22:58:42 +0000

    push

--- basic/alert.js
+++ basic/alert.js
@@ -1,2 +1,17 @@
 const score=400
 console.log(score);
+const number=new Number(100)
+console.log(number);
+
+console.log(number.toString().length)
+
+console.log(number.toFixed(3))
+const otherNumber=23.8966
+console.log(otherNumber.toPrecision(4))
+let b=new Date()
+console.log(b.toLocaleString())
+n=100000
+console.log(n.toLocaleString(1));
+console.log(typeof(b))
+const arr=[1,2,4,6];
+console.log(arr);
