34fa12b2fdb385ea90fc3a6cb81ac115fa224915
mukhiya8298600127 <157529067+mukhiya8298600127@users.noreply.github.com>
2025-01-19 21:38:54 +0000

    mycode of javascript

--- basic/w.js
+++ basic/w.js
@@ -6,3 +6,23 @@ console.log(getname);
 console.log(getname.length)
 console.log(getname.charAt(2))
 console.log(getname.indexOf('a'))
+console.log(getname.substring(-5,4))
+console.log(getname.slice(-5,3))
+const number='40'
+const number1=Number(number)
+console.log(typeof(number))
+console.log(typeof(number1))
+const n1=new Number(100)
+console.log(n1.toString.length)
+const nu=23.8966
+console.log(nu.toPrecision(4))
+const n3=1000000
+console.log(n3.toLocaleString('en-IN'))
+const arr=[1,2,3,4]
+console.log(arr)
+arr.push(6)
+console.log(arr)
+const arr1=new Array(1,2,3,4)
+console.log(arr1)
+arr1.push(7)
+console.log(arr1)
