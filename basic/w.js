c11516ead79623f99e82b4a37ed50e4d29bf2130
mukhiya8298600127 <157529067+mukhiya8298600127@users.noreply.github.com>
2025-01-19 18:17:41 +0000

    code

---
+++ basic/w.js
@@ -0,0 +1,8 @@
+const name="shashi"
+const repocount=50;
+console.log(name+repocount);
+const getname=new String("shashi");
+console.log(getname);
+console.log(getname.length)
+console.log(getname.charAt(2))
+console.log(getname.indexOf('a'))
